261897be5d3eb737b8d4db4ab235f87dc6da2fcb
Alex Standiford <alex@standiford.us>
2024-12-25 15:10:21 -0500

    Adds events

---
+++ lib/Events/RecordCreated.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace PHPNomad\Datastore\Events;
+
+use PHPNomad\Datastore\Interfaces\DataModel;
+use PHPNomad\Events\Interfaces\Event;
+
+class RecordCreated implements Event
+{
+    protected DataModel $record;
+
+    public function __construct(DataModel $record)
+    {
+        $this->record = $record;
+    }
+
+    public function getRecord(): DataModel
+    {
+        return $this->record;
+    }
+
+    public static function getId(): string
+    {
+        return 'record_created';
+    }
+}
